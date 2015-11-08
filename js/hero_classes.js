c7bf381e29ed4726e4bce6206e26ee0e4c54a6fe
mattheh <mattheh@email.unc.edu>
2015-11-07 22:35:11 -0500

    Fixes #4, add monster classes

--- js/hero_classes.js
+++
@@ -1,8 +0,0 @@
-//avg health: 100
-//avg attack 25
-var heroClasses = [
-    {class: "warrior", health: 150, attack: 25, img: "assets/images/warrior_idle.png"},
-    {class: "blackbelt", health: 120, attack: 35, img: "assets/images/blackbelt_idle.png"},
-    {class: "thief", health: 9, attack: 20, img: "assets/images/thief_idle.png"},
-    {class: "blackmage", health: 70, attack: 30, img: "assets/images/blackmage_idle.png"},
-    ]
