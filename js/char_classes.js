7adbdb893027888d66957fd537d169987c50f7ea
guanzo <guanzo91@gmail.com>
2015-11-08 21:33:44 -0500

    fixed broken char_classes.js

--- js/char_classes.js
+++ js/char_classes.js
@@ -2,34 +2,34 @@
 //avg attack: 25
 //avg run %: 30
 var heroClasses = [
-    {class: "warrior,
+    {class: "warrior",
 	 health: 150,
 	 attack: 30,
-     run: 30
+     run: 30,
 	 img: "assets/images/heroes/warrior_idle.png"},
-    {class: "blackbelt,
+    {class: "blackbelt",
 	 health: 120,
 	 attack: 35,
-     run: 35
+     run: 35,
 	 img: "assets/images/heroes/blackbelt_idle.png"},
-    {class: "thief,
+    {class: "thief",
 	 health: 90,
 	 attack: 25,
-     run: 45
+     run: 45,
 	 img: "assets/images/heroes/thief_idle.png"},
-    {class: "blackmage,
+    {class: "blackmage",
 	 health: 70,
 	 attack: 15,
-     run: 40
+     run: 40,
 	 img: "assets/images/heroes/blackmage_idle.png"},
     ]
 
 var monsterClasses = [
-    {class: "imp,
+    {class: "imp",
 	 health: 25,
 	 attack: 15,
 	 img: "assets/images/monsters/imp.png"},
-    {class: "wolf,
+    {class: "wolf",
 	 health: 20,
 	 attack: 20,
 	 img: "assets/images/monsters/wolf.png"}
