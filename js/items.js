dc7ca29551421b3893c5a28f1a444de2bef0e624
mattheh <mattheh@email.unc.edu>
2015-11-09 09:53:58 -0500

    Add Items JSON

---
+++ js/items.js
@@ -0,0 +1,24 @@
+//avg health: 100
+//avg attack: 25
+//avg run %: 30
+var consumables = [
+    {name: "Potion",
+	 description: "Restores 50 HP",
+	 affects: "hero"},
+    {name: "Antidote",
+	 description: "Cures Poison",
+	 affects: "hero"},
+    {name: "Hi-Potion",
+	 description: "Restores 150 HP",
+	 affects: "hero"},
+    {name: "Gold Needle",
+	 description: "Cures Petrify",
+	 affects: "hero"}
+    ]
+
+var weapons = [
+    ]
+var armor = [
+    ]
+var accessories = [
+    ]
